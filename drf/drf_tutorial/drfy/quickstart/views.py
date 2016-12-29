da8663778a59e7aaae85f8a6a58059fd4d4abec2
tufedtm <tufedtm@gmail.com>
2016-12-29 14:36:55 +0300

    + drf quickstart

--- drf/drf_tutorial/drfy/quickstart/views.py
+++ drf/drf_tutorial/drfy/quickstart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    uvs
+    """
+    queryset = User.objects.order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    gvs
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
