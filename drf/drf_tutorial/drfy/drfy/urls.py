e82e25b96f6fa0139fcfa4189855df48c53447ad
tufedtm <tufedtm@gmail.com>
2016-12-29 19:40:49 +0300

    + fbv

--- drf/drf_tutorial/drfy/drfy/urls.py
+++ drf/drf_tutorial/drfy/drfy/urls.py
@@ -22,7 +22,8 @@ router = routers.DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
+    url(r'^snippets/', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^admin/', admin.site.urls),
 ]
