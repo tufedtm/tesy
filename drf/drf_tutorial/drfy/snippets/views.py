70a53f4e99e9ec0c125d2a9a0c5178c4880790fc
tufedtm <tufedtm@gmail.com>
2016-12-30 15:26:17 +0300

    via only generics

--- drf/drf_tutorial/drfy/snippets/views.py
+++ drf/drf_tutorial/drfy/snippets/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework import mixins
 from rest_framework import viewsets
 
 from .models import Snippet
@@ -14,34 +13,17 @@ class SnippetViewSet(viewsets.ModelViewSet):
     serializer_class = SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
