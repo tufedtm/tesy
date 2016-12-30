03f658ab4ee46887874a25bbe1a44e4f4de5baea
tufedtm <tufedtm@gmail.com>
2016-12-30 14:40:33 +0300

    via @api_view + format suffix for urls

--- drf/drf_tutorial/drfy/drfy/urls.py
+++ drf/drf_tutorial/drfy/drfy/urls.py
@@ -1,21 +1,7 @@
-"""drfy URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
+
 from snippets import views
 
 router = routers.DefaultRouter()
@@ -23,7 +9,7 @@ router.register(r'snippets', views.SnippetViewSet)
 
 urlpatterns = [
     # url(r'^', include(router.urls)),
-    url(r'^snippets/', include('snippets.urls')),
+    url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^admin/', admin.site.urls),
 ]
