152d36ebc7286056238ed4f7a5ebdf069c904fc2
tufedtm <tufedtm@gmail.com>
2017-01-04 15:05:37 +0300

    + api_root via decorator api_view * serializers via HyperlinkedModelSerializer + snippet highlight + result paginate

--- drf/drf_tutorial/drfy/snippets/serializers.py
+++ drf/drf_tutorial/drfy/snippets/serializers.py
@@ -4,15 +4,16 @@ from rest_framework import serializers
 from .models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = '__all__'
+        fields = ('url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style')
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
     class Meta:
--- drf/drf_tutorial/drfy/snippets/views.py
+++ drf/drf_tutorial/drfy/snippets/views.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, viewsets, permissions
+from rest_framework import (
+    generics,
+    permissions,
+    renderers,
+    viewsets
+)
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Snippet
-from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -33,6 +49,16 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
