accc49bf4c5ee79cf6a4ac37d21223a1bece6d39
tufedtm <tufedtm@gmail.com>
2017-01-04 13:57:53 +0300

    + permission + user

--- drf/drf_tutorial/drfy/snippets/views.py
+++ drf/drf_tutorial/drfy/snippets/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
-from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework import generics, viewsets, permissions
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -19,6 +20,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -27,3 +32,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
