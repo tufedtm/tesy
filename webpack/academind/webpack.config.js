936e08a9bc35287ea65c70d4be61701ff4532eb6
tufedtm <tufedtm@gmail.com>
2017-06-11 22:13:11 +0300

    + wenpack

---
+++ webpack/academind/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin()]
+};
